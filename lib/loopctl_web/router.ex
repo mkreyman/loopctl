7a00452b13eab10981da7e70360a946554e1d733
FinanceAlex <3969010@gmail.com>
2026-07-22 13:09:27 -0600

    Add put_root_layout to the browser pipeline
    The :browser pipeline lacks put_root_layout (and fetch_live_flash), so on
    a fresh install /signup renders without the root layout: no CSS bundle,
    no flash, non-functional page. With the root layout plugged in, /signup
    renders and works as intended.
    
    Fixes mkreyman/loopctl issue 494.

--- lib/loopctl_web/router.ex
+++ lib/loopctl_web/router.ex
@@ -4,6 +4,8 @@ defmodule LoopctlWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, html: {LoopctlWeb.Layouts, :root}
     plug :protect_from_forgery
 
     plug :put_secure_browser_headers, %{
